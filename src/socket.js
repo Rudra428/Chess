e046cefe20477d9f6a82434e3802b89d055c79a8
Rudra428 <neelamnishad1792@gmail.com>
2026-03-11 20:33:38 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
 const socket = io("https://chess-backend-eg1b.onrender.com", {
-  transports: ["websocket", "polling"]
+  transports: ["websocket", "polling"],
+  reconnectionAttempts: 5,
+  reconnectionDelay: 2000,
 });
 
 export default socket;
